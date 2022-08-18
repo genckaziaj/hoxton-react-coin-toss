789c0c86ab8f60e8ab0222e4741ae1f83b00ce64
Genci23 <genckaziaj@yahoo.com>
2022-08-18 22:23:45 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [image, setImage] = useState({ src: "../public/assets/Front.jpg" });
+
+  function changeImage() {
+    const imageClone = structuredClone(image);
+
+    if (imageClone.src === "../public/assets/Front.jpg")
+      imageClone.src = "../public/assets/Back.jpg";
+    else imageClone.src = "../public/assets/Front.jpg";
+
+    setImage(imageClone);
+  }
+
+  return (
+    <div className="App">
+      <div className="coin">
+        <img className="image" src={image.src} />
+      </div>
+      <button
+        className="button"
+        onClick={() => {
+          changeImage();
+        }}
+      >
+        Flip Coin
+      </button>
+    </div>
+  );
+}
+
+export default App;
